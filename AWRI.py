4dfc2ec634fd285e557317d2672d86d3c6df4c40
staddlez <yapc@mail.gvsu.edu>
2019-09-07 17:17:20 -0400

    Updating AWRI V1

---
+++ AWRI.py
@@ -0,0 +1,105 @@
+#################################################################################
+# Name: Christian Yap
+# Description: Lincoln Peterson with Chapman's Estimator Program:
+# Estimates population using the Lincoln Peterson Chapman Estimator, as well as
+# allowing users to change assumed assumptions and parameters and get a simulation.
+# Version: 1.0, Created August 2019
+#################################################################################
+import random
+import sys
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import QDate, QTime, QDateTime, Qt
+from MainWindow import Ui_MainWindow
+from matplotlib.backends.backend_qt5agg import (NavigationToolbar2QT as NavigationToolbar)
+import numpy as np
+import random
+
+#################################################################################
+# CLASS FOR THE GUI
+#################################################################################
+class MainWindow(QMainWindow, Ui_MainWindow):
+
+    def __init__(self, *args, **kwargs):
+        # Declare the user interface:
+        super(MainWindow, self).__init__(*args, **kwargs)
+        self.setupUi(self)
+        self.Connections()
+        self.show()
+
+    #################################################################################
+    # Connections for the buttons
+    #################################################################################
+    def Connections(self):
+        self.estimatePopulationButton.clicked.connect(self.EstimatePopulationChapman)
+        self.runSimulationButton.clicked.connect(self.simulateFishes)
+
+    #################################################################################
+    # Lincoln Peterson Calculation
+    #################################################################################
+    def LincolnPetersonChapmanFormula(self):
+
+        markFirstCatchM = int(self.markedFirstCatchInput.value())
+        captureSecondCatchC = int(self.caughtSecondCatchInput.value())
+        markSecondCatchR = int(self.markedSecondCatchInput.value())
+
+        # Error Check: Marked second fishes cannot be greater than Marked First Fishes
+        if markSecondCatchR > markFirstCatchM:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Invalid Input:")
+            msg.setInformativeText("Amount of recaptured-marked fishes must be less than marked fishes from first catch")
+            msg.setWindowTitle("Error")
+            msg.exec_()
+
+        # Error Check: Marked second fishes cannot be greater than Captured Second Fishes
+        elif markSecondCatchR > captureSecondCatchC:
+            msg = QMessageBox()
+            msg.setIcon(QMessageBox.Critical)
+            msg.setText("Invalid Input:")
+            msg.setInformativeText("Number of marked fishes in second catch cannot be greater than number of "
+                                   "recaptured fish")
+            msg.setWindowTitle("Error")
+            msg.exec_()
+
+        # Else we are good:
+        else:
+            estimatedSampleSizeN = (((markFirstCatchM + 1) * (captureSecondCatchC + 1)) / (markSecondCatchR + 1)) - 1
+            # Date and Time set:
+            dateNow = QTime.currentTime().toString()
+            self.resultScreenOne.append(dateNow + " - Estimated Fish Population: " + str(estimatedSampleSizeN))
+            # QMessageBox.information(self, "A Good Message", "Success. Results shown in the results box.")
+
+    #################################################################################
+    # Estimate Population using Lincoln Peterson's Chapman Model - TAB ONE
+    #################################################################################
+    def EstimatePopulationChapman(self):
+        self.LincolnPetersonChapmanFormula()
+
+    #################################################################################
+    # SIMULATION - TAB TWO
+    #################################################################################
+    def simulateFishes(self):
+        QMessageBox.information(self, "A Good Message", "Success. Results shown in the results box.")
+        fs = 500
+        f = random.randint(1, 100)
+        ts = 1 / fs
+        length_of_signal = 100
+        t = np.linspace(0, 1, length_of_signal)
+
+        cosinus_signal = np.cos(2 * np.pi * f * t)
+        sinus_signal = np.sin(2 * np.pi * f * t)
+        self.MplWidget.canvas.axes.clear()
+        self.MplWidget.canvas.axes.plot(t, cosinus_signal)
+        self.MplWidget.canvas.axes.plot(t, sinus_signal)
+        self.MplWidget.canvas.axes.legend(('cosinus', 'sinus'), loc='upper right')
+        self.MplWidget.canvas.axes.set_title('Cosinus - Sinus Signal')
+        self.MplWidget.canvas.draw()
+
+#################################################################################
+# MAIN FUNCTION
+#################################################################################
+if __name__ == "__main__":
+    app = QApplication([])
+    ui = MainWindow()
+    sys.exit(app.exec_())
